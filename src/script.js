6e9776058ddf2430b3e1332b3c9b0d22dd7dde82
Terrance Li <terranceli.terbear@gmail.com>
2021-01-09 10:42:06 -0800

    Reorganize position update code

--- src/script.js
+++ src/script.js
@@ -1,8 +1,32 @@
 // Initialize three.js scene
 const scene = new THREE.Scene();
 
+// Configure protagonist
+const CHICKEN_INITIAL_POSITION = {
+  x: 0,
+  y: 0,
+  z: 0
+}
+
+const CHICKEN_SIZE = 30;
+const CHICKEN_SPEED = 5;
+const chicken = new Chicken();
+
+scene.add(chicken);
+chicken.position = CHICKEN_INITIAL_POSITION
+
+const target = {
+  x: 0,
+  y: 0
+}
+
 // Configure camera
 const DISTANCE = 500;
+const CAMERA_OFFSET = {
+  x: 0,
+  y: -DISTANCE / 2,
+  z: DISTANCE
+}
 
 const camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000);
 
@@ -10,25 +34,17 @@ camera.rotation.x = 30 * Math.PI / 180;
 camera.rotation.y = 0 * Math.PI / 180;
 camera.rotation.z = 0 * Math.PI / 180;
 
-camera.position.y = -DISTANCE / 2;
-camera.position.x = 0;
-camera.position.z = DISTANCE;
-
-// Configure protagonist
-const CHICKEN_SIZE = 30;
-const CHICKEN_SPEED = 5;
-const chicken = new Chicken();
-
-scene.add(chicken);
-chicken.position.x = 0;
-chicken.position.y = 0;
-
-const target = {
-  x: 0,
-  y: 0
-}
+camera.position.x = CHICKEN_INITIAL_POSITION.x + CAMERA_OFFSET.x;
+camera.position.y = CHICKEN_INITIAL_POSITION.y + CAMERA_OFFSET.y;
+camera.position.z = CHICKEN_INITIAL_POSITION.z + CAMERA_OFFSET.z;
 
 // Configure lighting
+const DIR_LIGHT_OFFSET = {
+  x: 100,
+  y: 200,
+  z: 200
+}
+
 hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
 scene.add(hemiLight)
 
@@ -36,8 +52,8 @@ dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
 dirLight.castShadow = true;
 scene.add(dirLight);
 
-dirLight.shadow.mapSize.width = 2048;
-dirLight.shadow.mapSize.height = 2048;
+dirLight.shadow.mapSize.width = 1024;
+dirLight.shadow.mapSize.height = 1024;
 
 const d = 100;
 dirLight.shadow.camera.left = -d;
@@ -45,9 +61,9 @@ dirLight.shadow.camera.right = d;
 dirLight.shadow.camera.top = d;
 dirLight.shadow.camera.bottom = -d;
 
-dirLight.position.x = 100;
-dirLight.position.y = 200;
-dirLight.position.z = 200;
+dirLight.position.x = CHICKEN_INITIAL_POSITION.x + DIR_LIGHT_OFFSET.x;
+dirLight.position.y = CHICKEN_INITIAL_POSITION.y + DIR_LIGHT_OFFSET.y;
+dirLight.position.z = CHICKEN_INITIAL_POSITION.z + DIR_LIGHT_OFFSET.z;
 dirLight.target = chicken
 
 // Generate grass
@@ -66,6 +82,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+function move(dx, dy) {
+  chicken.position.x += dx
+  chicken.position.y += dy
+
+  camera.position.x = chicken.position.x
+  camera.position.y = chicken.position.y - DISTANCE / 2
+
+  dirLight.position.x = chicken.position.x + 100
+  dirLight.position.y = chicken.position.y + 200
+}
+
 function animate(timestamp) {
   requestAnimationFrame(animate);
 
@@ -80,14 +107,7 @@ function animate(timestamp) {
       dy = (target.y - chicken.position.y)
     }
 
-    chicken.position.x += dx
-    chicken.position.y += dy
-
-    camera.position.x = chicken.position.x
-    camera.position.y = chicken.position.y - DISTANCE / 2
-
-    dirLight.position.x = chicken.position.x + 100
-    dirLight.position.y = chicken.position.y + 200
+    move(dx, dy)
   }
   renderer.render(scene, camera);	
 }
