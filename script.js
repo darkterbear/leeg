da4e9bd49e4e6cd4be1131970de63f524cea0e4a
Terrance Li <terranceli.terbear@gmail.com>
2021-01-08 23:01:24 -0800

    Chicken can move

--- script.js
+++ script.js
@@ -14,24 +14,25 @@ camera.position.y = -distance / 2;
 camera.position.x = 0;
 camera.position.z = distance;
 
-const zoom = 2;
-
 // Configure protagonist
-const chickenSize = 15;
-
+const chickenSize = 30;
+const speed = 10;
 const chicken = new Chicken();
 scene.add(chicken);
+
+const target = {
+  x: 0,
+  y: 0
+}
 
 // Configure lighting
 hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
 scene.add(hemiLight)
 
-const initialDirLightPositionX = 100;
-const initialDirLightPositionY = 100;
 dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
-dirLight.position.set(initialDirLightPositionX, initialDirLightPositionY, 200);
+dirLight.position.set(0, 0, 200);
 dirLight.castShadow = true;
-dirLight.target = chicken;
+// dirLight.target = chicken;
 scene.add(dirLight);
 
 dirLight.shadow.mapSize.width = 2048;
@@ -42,11 +43,6 @@ dirLight.shadow.camera.right = d;
 dirLight.shadow.camera.top = d;
 dirLight.shadow.camera.bottom = - d;
 
-backLight = new THREE.DirectionalLight(0x000000, .4);
-backLight.position.set(200, 200, 50);
-backLight.castShadow = true;
-scene.add(backLight)
-
 const initaliseValues = () => {
   // Generate grass
   const grass = new Grass();
@@ -55,8 +51,8 @@ const initaliseValues = () => {
   chicken.position.x = 0;
   chicken.position.y = 0;
 
-  dirLight.position.x = initialDirLightPositionX;
-  dirLight.position.y = initialDirLightPositionY;
+  dirLight.position.x = 100;
+  dirLight.position.y = 200;
 }
 
 initaliseValues();
@@ -77,14 +73,14 @@ function Grass() {
   const grass = new THREE.Group();
 
   const createSection = color => new THREE.Mesh(
-    new THREE.BoxBufferGeometry(1000*zoom, 300*zoom, 3*zoom ), 
-    new THREE.MeshPhongMaterial( { color } )
+    new THREE.BoxBufferGeometry(2000, 600, 6), 
+    new THREE.MeshPhongMaterial({ color })
   );
 
   const main = createSection(0xbaf455);
   main.receiveShadow = true;
   grass.add(main);
-  grass.position.z = 1.5*zoom;
+  grass.position.z = 3;
 
   this.mesh = grass;
 }
@@ -93,19 +89,21 @@ function Chicken() {
   const chicken = new THREE.Group();
 
   const body = new THREE.Mesh(
-    new THREE.BoxBufferGeometry( chickenSize*zoom, chickenSize*zoom, 20*zoom ), 
+    new THREE.BoxBufferGeometry( chickenSize, chickenSize, 40), 
     new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } )
   );
-  body.position.z = 10*zoom;
+  
+  body.position.z = 20;
   body.castShadow = true;
   body.receiveShadow = true;
   chicken.add(body);
 
   const rowel = new THREE.Mesh(
-    new THREE.BoxBufferGeometry( 2*zoom, 4*zoom, 2*zoom ), 
+    new THREE.BoxBufferGeometry(4, 8, 4), 
     new THREE.MeshLambertMaterial( { color: 0xF0619A, flatShading: true } )
   );
-  rowel.position.z = 21*zoom;
+
+  rowel.position.z = 42;
   rowel.position.y += 10
   rowel.castShadow = true;
   rowel.receiveShadow = false;
@@ -117,7 +115,21 @@ function Chicken() {
 function animate(timestamp) {
   requestAnimationFrame( animate );
 
-  // console.log('hi')
+  // console.log(target, )
+  const distToTarget = Math.sqrt((target.x - chicken.position.x) ** 2 + (target.y - chicken.position.y) ** 2)
+  
+  if (distToTarget > 1) {
+    dx = (target.x - chicken.position.x) / distToTarget * speed
+    dy = (target.y - chicken.position.y) / distToTarget * speed
+
+    if (distToTarget < speed) {
+      dx = (target.x - chicken.position.x)
+      dy = (target.y - chicken.position.y)
+    }
+
+    chicken.position.x += dx
+    chicken.position.y += dy
+  }
   renderer.render( scene, camera );	
 }
 
@@ -127,6 +139,8 @@ window.addEventListener('contextmenu', (e) => {
   y = -e.clientY / Math.sqrt(3) * 2 + window.innerHeight / 2 + 110
 
   chicken.rotation.z = Math.atan2(y - chicken.position.y, x - chicken.position.x) - Math.PI / 2
+  target.x = x
+  target.y = y
 })
 
 requestAnimationFrame( animate );
